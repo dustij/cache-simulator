de2e7753b76d42012c7b349a33b82a266ee4205f
dustij <dev.dusti@gmail.com>
2025-05-08 12:00:58 -0500

    made the bones

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,25 @@
-import { Container } from "@/components/Container";
-import Image from "next/image";
+import { debugging } from "@/lib/constants";
+import { cn } from "@/lib/utils";
 
 export default function Home() {
-    return (
-        <Container className="mt-9">
-            <div className="font-[family-name:var(--font-geist-sans)] max-w-2xl">
-                <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-                    Cache Simulator
-                </h1>
-                <main className=""></main>
-                <footer className=""></footer>
-            </div>
-        </Container>
-    );
+  return (
+    <div
+      className={cn(
+        "flex flex-grow flex-col items-center gap-6 py-6",
+        debugging ? "bg-amber-300" : "",
+      )}
+    >
+      <header className="max-w-2xl">
+        <h1 className="text-4xl font-bold tracking-tighter text-balance text-zinc-800 sm:text-5xl dark:text-zinc-100">
+          Cache Simulator
+        </h1>
+      </header>
+      <main
+        className={cn(
+          "min-h-[720px] min-w-[950px]",
+          debugging ? "bg-green-300" : "",
+        )}
+      ></main>
+    </div>
+  );
 }
