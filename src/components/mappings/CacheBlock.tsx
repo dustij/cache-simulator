5e8be2ccf48da1f719f9692acddc80b708340e1b
dustij <dev.dusti@gmail.com>
2025-05-14 19:38:21 -0500

    clean up some commented out stuff

--- src/components/mappings/CacheBlock.tsx
+++ src/components/mappings/CacheBlock.tsx
@@ -32,34 +32,16 @@ export function CacheBlock({
     config.blockSize,
   );
 
-  // console.log("#################");
   const blockData = [];
   for (let i = 0; i < size; i++) {
     const isCurrentBlock = !initial && block % config.cacheBlocks === index;
-    // const isCurrentBlock = !initial && index === lineIndex;
     const isValid = !initial && config.cacheLines[index] !== null;
-    // console.log("================");
-    // console.log("line#: " + i);
-    // console.log("cache#: " + index);
-    // console.log("block#: " + block);
-    // console.log(config.cacheLines);
-    // console.log("is valid: " + isValid);
-    // const cellClass = isCurrentBlock
-    //   ? offset === i
-    //     ? "bg-zinc-900"
-    //     : "bg-zinc-400"
-    //   : isValid
-    //     ? "bg-zinc-400"
-    //     : "bg-zinc-300";
     let cellClass = isValid ? "bg-zinc-400" : "bg-zinc-300";
     if (isCurrentBlock) {
       cellClass = i === offset ? "bg-zinc-900" : "bg-zinc-400";
     }
 
-    blockData.push(
-      // <div key={i} className={cn("h-full bg-zinc-300", cellClass)}></div>,
-      <div key={i} className={cn("h-full", cellClass)}></div>,
-    );
+    blockData.push(<div key={i} className={cn("h-full", cellClass)}></div>);
   }
 
   return (
